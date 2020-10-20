f9f1e6f4328dff89721d9bd26633c54c546ad0e0
Devco2011 <devco2011@gmail.com>
2020-10-19 19:18:33 -0500

    login and registration are functiional

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom"
 import './index.css';
-import App from './App';
+import { TidyFamily } from "./components/TidyFamily.js";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <TidyFamily />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
