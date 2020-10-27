1923e2a1cfe04907105ea34a8e48d90091bc64ec
Devco2011 <devco2011@gmail.com>
2020-10-27 11:14:22 -0500

    FamMemberPointsList is working

--- src/components/points/FamMemberPointsList.js
+++ src/components/points/FamMemberPointsList.js
@@ -1,44 +1,60 @@
 import React, { useContext, useEffect } from "react"
 import { ChoreContext } from "../chores/ChoreProvider"
+import { FamMemberContext } from "../famMembers/FamMemProvider"
 import { useHistory } from "react-router-dom"
 
 export const FamMemberPointsList = () => {
     const { chores, getChores } = useContext(ChoreContext)
+    const { famMembers, getFamMembers } = useContext(FamMemberContext)
     const history = useHistory()
-    let famMembers = [];
+    let familyMembers = [];
     let points = [];
     const intitialValue = 0;
 
     useEffect(() => {
         getChores()
+        getFamMembers()
 
     }, [])
 
 
     const getFamMemberChores = (familyMemberId) => {
 
-        return famMembers = chores.filter(chore => chore.familyMemberId === familyMemberId)
+        return familyMembers = chores.filter(chore => chore.familyMemberId === familyMemberId)
 
     }
-    getFamMemberChores(2)
-    console.log(famMembers)
+
+
 
     const getFamMemberPoints = () => {
 
-        return points = famMembers.map((famMember) => famMember.pointsValue)
+        return points = familyMembers.map((familyMember) => familyMember.pointsValue)
 
     }
-    console.log(getFamMemberPoints())
+
 
     const reducer = (accumulator, item) => {
         return accumulator + item;
 
     }
-    const totalPoints = points.reduce(reducer, intitialValue)
-    console.log(totalPoints)
+    const totalPoints = () => points.reduce(reducer, intitialValue)
+
 
     return (
         <>
+            <div className="famMembers">
+                {famMembers.map(famMember => {
+                    if (famMember?.familyId === parseInt(localStorage.getItem("family_id"))) {
+
+                        getFamMemberChores(famMember.id)
+                        console.log(familyMembers)
+                        getFamMemberPoints()
+                        console.log(points)
+                        return <h2>Current Points: {totalPoints()}</h2>
+                    }
+                })
+                }
+            </div>
             <h2>Current Points: {totalPoints}</h2>
 
 
